d76abead5e1c443837f254242b8f5401786722c9
mahedi6666655555 <mahedinir34678@gmail.com>
2023-06-13 01:53:43 +0600

    interview question

---
+++ index.js
@@ -0,0 +1,44 @@
+const quto = "my name is mahedi he is mahedi he is a good programmer mahedi"
+
+
+// defination steps 
+
+//     * first the variable name 
+
+// * replece keyword for the change
+
+//     * then refuler expresson / what is you want to change / g=> is used for the global g means global / mahedi / g,   
+
+// * last step set the "sentense " you want to replace by the sentanse
+
+
+// console.log(quto.replace(/mahedi/g,"king"));
+
+// console.log({quto});
+
+
+
+const start = performance.now() // its get the start time 
+
+for (let i = 0; i < 50; i++) {
+
+    console.log(i);
+}
+
+const ends = performance.now() // its get the ends time 
+    // <!-- clg and - teh valuse -->
+
+
+    * // then - teh two valus first wrap use it {
+        
+    const start = performance.now()
+
+// what you want to get the time
+
+const ends = performance.now()
+
+//} 
+
+
+console.log(`start time ${start - ends}this is the end time`);
+
